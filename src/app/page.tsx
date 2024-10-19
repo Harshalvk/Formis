0282f84ebb7a7356641e7f72ff7d1607b81703fe
Harshalvk <harshalvkhobragade@gmail.com>
2024-10-19 10:52:45 +0530

    ADD: Form-generation component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
+import FormGenerator from "@/components/FormGenerator";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <Button>Create a Form</Button>
+      <FormGenerator/>
     </main>
   );
 }
