5f6477c13a52c6705d2e0e235e0def79b289f5b1
Harshalvk <harshalvkhobragade@gmail.com>
2024-10-19 12:05:41 +0530

    ADD: setup authentication using next-auth

--- environment.d.ts
+++ environment.d.ts
@@ -3,6 +3,8 @@ declare global {
     interface ProcessEnv {
       NODE_ENV: "development" | "production";
       GROQ_API_KEY: string;
+      GOOGLE_CLIENT_ID: string;
+      GOOGLE_CLIENT_SECRET: string;
     }
   }
 }
