737ae5f61e7a9f87d5eec94d81aac678d7977371
Harshalvk <harshalvkhobragade@gmail.com>
2024-10-20 18:19:23 +0530

    ADD: Save generated data to DB

--- src/app/actions/generateForm.ts
+++ src/app/actions/generateForm.ts
@@ -2,6 +2,7 @@
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 import Groq from "groq-sdk";
+import { saveForm } from "./mutateForm";
 
 export async function generateForm(
   prevState: {
@@ -26,7 +27,7 @@ export async function generateForm(
 
   const data = parse.data;
   const promptExplanation =
-    "Based on the description, generate a survey object with 3 fields: name(string) for the form, description(string) of the form and a questions array where every element has 2 fields: text and the fieldType and fieldType can be of these options RadioGroup, Select, Input, Textarea, Switch; and return it in json format. For RadioGroup, and Select types also return fieldOptions array with text and value fields. For example, for RadioGroup, and Select types, the field options array can be [{text: 'Yes', value: 'yes'}, {text: 'No', value: 'no'}] and for Input, Textarea, and Switch types, the field options array can be empty. For example, for Input, Textarea, and Switch types, the field options array can be []";
+    "PLEASE DO NOT START YOUR RESPONSE WITH WORDS AND DESCRIPTION LIKE 'Here is' etc, ALSO DO NOT WRAP THE RESPONSE INSIDE `` like this ```YOUR_RESPONSE```. MAKE SURE EACH KEY VALUE PAIR SHOULD BE WRAP IN DOUBLE QUOTES. Based on the description, generate a survey object with 3 fields: name(string) for the form, description(string) of the form and a questions array where every element has 2 fields: text and the fieldType and fieldType can be of these options RadioGroup, Select, Input, Textarea, Switch; and return it in json format. For RadioGroup, and Select types also return fieldOptions array with text(text should wrap in double quotes) and value(value should wrap in double quotes) fields. For example, for RadioGroup, and Select types, the field options array can be [{text: 'Yes', value: 'yes'}, {text: 'No', value: 'no'}] and for Input, Textarea, and Switch types, the field options array can be empty. For example, for Input, Textarea, and Switch types, the field options array can be [].";
 
   try {
     const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
@@ -40,11 +41,18 @@ export async function generateForm(
       ],
     });
 
+    const paresRes = JSON.parse(response.choices[0]?.message.content!);
+
+    const dbFormId = await saveForm({
+      name: paresRes.name,
+      description: paresRes.description,
+      questions: paresRes.questions,
+    });
+
     revalidatePath("/");
-    console.log(response.choices[0]?.message?.content);
     return {
       message: "success",
-      data: response.choices[0]?.message?.content || "",
+      data: {formId: dbFormId},
     };
   } catch (error) {
     console.log(error);
