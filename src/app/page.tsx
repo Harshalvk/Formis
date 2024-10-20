27a1fec0b88fab3a5ff415507dbd8a2b3f9a868a
Harshalvk <harshalvkhobragade@gmail.com>
2024-10-20 13:10:50 +0530

    ADD: Card component for forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import FormGenerator from "@/components/FormGenerator";
+import FormList from "@/components/forms/FormList";
 import Header from "@/components/Header";
+import { db } from "@/db";
 
-export default function Home() {
+export default async function Home() {
+  const forms = await db.query.forms.findMany();
   return (
     <>
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-center">
+      <main className="flex min-h-screen flex-col items-center">
         <FormGenerator />
+        <FormList forms={forms} />
       </main>
     </>
   );
