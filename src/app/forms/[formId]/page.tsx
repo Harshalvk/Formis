4ddd55f623df565654b57925ad9cf9661d5bcbde
Harshalvk <harshalvkhobragade@gmail.com>
2024-10-24 23:02:12 +0530

    UI: page to submit form

---
+++ src/app/forms/[formId]/page.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { db } from "@/db";
+import { forms } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { auth } from "@/auth";
+import Form from "@/components/forms/Form";
+
+const page = async ({
+  params,
+}: {
+  params: {
+    formId: string;
+  };
+}) => {
+  const session = await auth();
+  const formId = params.formId;
+
+  if (!formId) {
+    return <div>Form not found</div>;
+  }
+
+  const form = await db.query.forms.findFirst({
+    where: eq(forms.id, parseInt(formId)),
+    with: {
+      questions: {
+        with: {
+          fieldOptions: true,
+        },
+      },
+    },
+  });
+
+  if (session?.user?.id !== form?.userId) {
+    return <div>You are not authorized to view this page.</div>;
+  }
+
+  if (!form) {
+    return <div>You don't have any form</div>;
+  }
+
+  return (
+    <>
+      <Form form={form} />
+    </>
+  );
+};
+
+export default page;
