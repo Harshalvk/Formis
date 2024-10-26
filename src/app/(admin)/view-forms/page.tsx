ffcf43d19f458756fad99ddcb0944dd2362128d7
Harshalvk <harshalvkhobragade@gmail.com>
2024-10-26 22:16:03 +0530

    FIX: remove heading text

--- src/app/(admin)/view-forms/page.tsx
+++ src/app/(admin)/view-forms/page.tsx
@@ -8,7 +8,6 @@ const page = async () => {
   const forms: InferSelectModel<typeof dbForms>[] = await getUserForms();
   return (
     <>
-      <h1 className="text-3xl font-bold px-4 m-5">My Forms</h1>
       <FormList forms={forms} />
     </>
   );
