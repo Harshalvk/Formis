21c4a954b5e04f178f5e7de54960b1a85603eb43
Harshalvk <harshalvkhobragade@gmail.com>
2024-11-10 21:04:15 +0530

    ADD: landing page component

---
+++ src/components/landing-page/LandingPage.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import FormGenerator from "../FormGenerator";
+import { useTheme } from "next-themes";
+
+type Props = {};
+
+const LandingPage = (props: Props) => {
+  const [mounted, setMounted] = useState(false);
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  });
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <section className="flex flex-col items-center justify-center space-y-4 sm:pt-28 w-full bg-[url('/grid-black.svg')] dark:bg-[url('/grid-white.svg')] relative bg-opacity-5">
+      <div
+        className={`w-full absolute inset-0 bg-gradient-to-c from-transparent via-transparent z-0 ${
+          theme === "dark" ? "to-zinc-950" : "to-white"
+        }`}
+      />
+      <h1 className="text-4xl text-center font-semibold sm:text-5xl md:text-6xl tracking-tighter leading-6">
+        Forms built to grow with you,
+        <br /> transforming data into clear insights.
+      </h1>
+      <p className="mt-2 sm:text-xl max-w-2xl mx-auto text-center text-zinc-600 dark:text-zinc-300">
+        Generate and share your form in seconds. Explore rich analytics, visual
+        charts, and actionable insights.
+      </p>
+      <FormGenerator />
+      <div
+        className={`w-full bg-gradient-to-b from-transparent z-0${
+          theme === "dark" ? "to-zinc-950" : "to-white"
+        } h-24`}
+      />
+    </section>
+  );
+};
+
+export default LandingPage;
