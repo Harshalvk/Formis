5a7c3c4d83140eda71f42c24349bbf7f8d308218
Harshalvk <harshalvkhobragade@gmail.com>
2024-11-14 09:32:52 +0530

    ADD: Subscribe btn & setup

--- src/components/navigation/UpgradeAccBtn.tsx
+++ src/components/navigation/UpgradeAccBtn.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import Link from "next/link";
 import { getUserForms } from "@/app/actions/getUserForms";
 import { MAX_FREE_FORMS } from "@/lib/utils";
 import ProgressBar from "../ProgressBar";
+import SubscribeBtn from "../Subscription/SubscribeBtn";
+import { auth } from "@/auth";
 
 type Props = {};
 
@@ -10,6 +11,8 @@ const UpgradeAccBtn = async (props: Props) => {
   const forms = await getUserForms();
   const formCount = forms.length;
   const percent = (formCount / MAX_FREE_FORMS) * 100;
+  const session = await auth();
+  const userId = session?.user?.id;
 
   return (
     <div className="p-4 mb-4 text-sm">
@@ -18,9 +21,7 @@ const UpgradeAccBtn = async (props: Props) => {
         {formCount} out of {MAX_FREE_FORMS} forms generated
       </p>
       <p>
-        <Link href={"/setting"} className="underline">
-          Upgrade your plan
-        </Link>{" "}
+        <SubscribeBtn userId={userId} price={22} />
         for unlimited forms.
       </p>
     </div>
