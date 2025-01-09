9ef71d8ae22472e01601f0ed03cdd0b03bff7b6d
Harshalvk <harshalvkhobragade@gmail.com>
2025-01-09 22:41:19 +0530

    feat: stripe webhook to monitor stripe-webhook events

---
+++ src/app/api/stripe/webhook/route.ts
@@ -0,0 +1,53 @@
+import {
+  createSubscription,
+  deleteSubscription,
+} from "@/app/actions/userSubscriptions";
+import { stripe } from "@/lib/stripe";
+import Stripe from "stripe";
+
+const relevantEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+  "customer.subscription.created",
+]);
+
+export async function POST(req: Request) {
+  const body = await req.text();
+  const sig = req.headers.get("stripe-signature") as string;
+
+  if (!sig) return null;
+
+  if (!process.env.STRIPE_WEBHOOK_SECRET) {
+    throw new Error("STRIPE_WEBHOOK_SECRET is not set");
+  }
+
+  const event = stripe.webhooks.constructEvent(
+    body,
+    sig,
+    process.env.STRIPE_WEBHOOK_SECRET!
+  );
+
+  const data = event.data.object as Stripe.Subscription;
+
+  if (relevantEvents.has(event.type)) {
+    switch (event.type) {
+      case "customer.subscription.created": {
+        await createSubscription({
+          userId: data.customer as string,
+        });
+        break;
+      }
+      case "customer.subscription.deleted": {
+        await deleteSubscription({
+          userId: data.customer as string,
+        });
+        break;
+      }
+      default:
+        break;
+    }
+  }
+
+  return new Response(JSON.stringify({ received: true }), { status: 200 });
+}
