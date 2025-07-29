fd6d96ed3f31950a8d0534589bc36429ec3c031f
Harshalvk <harshalvkhobragade@gmail.com>
2025-07-30 00:05:38 +0530

    fix: payment success page

--- src/app/payment/success/page.tsx
+++ src/app/payment/success/page.tsx
@@ -4,16 +4,18 @@ import Link from "next/link";
 
 const page = () => {
   return (
-    <Alert variant="default">
-      <AlertTitle>Success</AlertTitle>
-      <AlertDescription>
-        Your account has been updated.{" "}
-        <Link href="/view-forms" className="underline">
-          Go to the dashboard
-        </Link>{" "}
-        to create more forms
-      </AlertDescription>
-    </Alert>
+    <div className="h-screen nax-w-4xl flex items-center justify-center">
+      <Alert variant="default" className="max-w-4xl">
+        <AlertTitle>Success</AlertTitle>
+        <AlertDescription>
+          Your account has been updated.{" "}
+          <Link href="/view-forms" className="underline">
+            Go to the dashboard
+          </Link>{" "}
+          to create more forms
+        </AlertDescription>
+      </Alert>
+    </div>
   );
 };
 
