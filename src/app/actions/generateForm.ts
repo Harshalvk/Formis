fa8fe89a369bbbb317477f4819a300ea4069f80a
Harshalvk <harshalvkhobragade@gmail.com>
2025-07-31 18:59:54 +0530

    fix: redirect user if not logged in from generateForm server action

--- src/app/actions/generateForm.ts
+++ src/app/actions/generateForm.ts
@@ -5,13 +5,18 @@ import { z } from "zod";
 import Groq from "groq-sdk";
 import { saveForm } from "./mutateForm";
 import { promptSchema } from "@/lib/validator/prompt.vaildator";
+import { auth, signIn } from "@/auth";
 
 export async function generateForm({
   data
 }: {
   data: z.infer<typeof promptSchema>;
 }) {
-  console.log("PROMPT:::", data.prompt);
+  const session = await auth();
+
+  if (!session?.user) {
+    await signIn("google");
+  }
 
   const promptExplanation =
     "PLEASE DO NOT START YOUR RESPONSE WITH WORDS AND DESCRIPTION LIKE 'Here is' etc, ALSO DO NOT WRAP THE RESPONSE INSIDE `` like this ```YOUR_RESPONSE```. MAKE SURE EACH KEY VALUE PAIR SHOULD BE WRAP IN DOUBLE QUOTES. Based on the description, generate a survey object with 3 fields: name(string) for the form, description(string) of the form and a questions array where every element has 2 fields: text and the fieldType and fieldType can be of these options RadioGroup, Select, Input, Textarea, Switch; and return it in json format. For RadioGroup, and Select types also return fieldOptions array with text(text should wrap in double quotes) and value(value should wrap in double quotes) fields. For example, for RadioGroup, and Select types, the field options array can be [{text: 'Yes', value: 'yes'}, {text: 'No', value: 'no'}] and for Input, Textarea, and Switch types, the field options array can be empty. For example, for Input, Textarea, and Switch types, the field options array can be [].";
